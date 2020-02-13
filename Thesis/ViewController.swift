53ab6d183c75f2638528778314c3535c8ecafacb
Makc0n <makc0n47@gmail.com>
2020-02-14 00:28:17 +0300

    Initial Commit

---
+++ Thesis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Thesis
+//
+//  Created by Максим Василаки on 14.02.2020.
+//  Copyright © 2020 Максим Василаки. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
