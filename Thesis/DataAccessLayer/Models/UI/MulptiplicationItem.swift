c16292b2bd634e94870c7d7d512b079286dbe9e4
Makc0n <>
2021-01-30 01:10:27 +0300

    test

---
+++ Thesis/DataAccessLayer/Models/UI/MulptiplicationItem.swift
@@ -0,0 +1,18 @@
+//
+//  MulptiplicationItem.swift
+//  Thesis
+//
+//  Created by Максим Василаки on 30.01.2021.
+//  Copyright © 2021 Максим Василаки. All rights reserved.
+//
+
+import Foundation
+
+enum MultiplicationItem {
+    
+    case quest
+    case answer
+    case attempt
+    
+    
+}
